f9aec4fe7aba209d87ab42e5cd4bc340305cf5ea
adrianzap <adrianzap24@gmail.com>
2018-06-14 18:11:04 +0200

    Use spaces instead of tabs in Judicious py files

--- JudiciousPartitioning/Elem.py
+++ JudiciousPartitioning/Elem.py
@@ -1,33 +1,33 @@
 class Elem:
-	def __init__(self, name, contained_set, contained_vertices=None):
-		self.name = name
-		self.contained_set = contained_set
-		# Only used for E
-		self.contained_vertices = contained_vertices
+    def __init__(self, name, contained_set, contained_vertices=None):
+        self.name = name
+        self.contained_set = contained_set
+        # Only used for E
+        self.contained_vertices = contained_vertices
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	def __hash__(self):
-		return hash(self.name)
+    def __hash__(self):
+        return hash(self.name)
 
-	def __eq__(self, other):
-		return self.contained_set == other.contained_set
+    def __eq__(self, other):
+        return self.contained_set == other.contained_set
 
-	def __ne__(self, other):
-		return not self == other
+    def __ne__(self, other):
+        return not self == other
 
-	def __iter__(self):
-		return iter(self.contained_set)
+    def __iter__(self):
+        return iter(self.contained_set)
 
-	def __next__(self):
-		return next(self.contained_set)
+    def __next__(self):
+        return next(self.contained_set)
 
-	def __sub__(self, other):
-		if isinstance(other, set):
-			return self.contained_set - other
-		else:
-			return NotImplemented
+    def __sub__(self, other):
+        if isinstance(other, set):
+            return self.contained_set - other
+        else:
+            return NotImplemented
 
-	def __len__(self):
-		return len(self.contained_set)
+    def __len__(self):
+        return len(self.contained_set)
--- JudiciousPartitioning/main.py
+++ JudiciousPartitioning/main.py
@@ -7,141 +7,140 @@ from Elem import Elem
 
 
 def parse_arguments():
-	parser = argparse.ArgumentParser(description="Split one partition of a MSA to k CPUs just by splitting the sites "
-												 "into k blocks.")
+    parser = argparse.ArgumentParser(description="Split one partition of a MSA to k CPUs just by splitting the sites "
+                                                 "into k blocks.")
 
-	parser.add_argument('partition_file', help="the partitions file you want to split")
-	parser.add_argument('k', type=int, help="the number of blocks you want to split the partition into")
+    parser.add_argument('partition_file', help="the partitions file you want to split")
+    parser.add_argument('k', type=int, help="the number of blocks you want to split the partition into")
 
-	args = parser.parse_args()
-	return args
+    args = parser.parse_args()
+    return args
 
 
 def generate_T(H, cm_plus_d):
-	return set(itertools.combinations(H, cm_plus_d))
+    return set(itertools.combinations(H, cm_plus_d))
 
 
 def generate_S(T, E):
-	S = set()
-	# for all sets in T create a set in S
-	for t in T:
-		S_j = set()
-		# in the set in S are all e that are a subset of t
-		for e in E:
-			if all(x in t for x in e):
-				S_j.add(e)
-		S.add(Elem("S_" + "".join(map(str, S_j)), S_j))
+    S = set()
+    # for all sets in T create a set in S
+    for t in T:
+        S_j = set()
+        # in the set in S are all e that are a subset of t
+        for e in E:
+            if all(x in t for x in e):
+                S_j.add(e)
+        S.add(Elem("S_" + "".join(map(str, S_j)), S_j))
 
-	return S
+    return S
 
 
 def find_minimum_set_in_S_covering_E(E, S):
-	P = set()
-	result = set()
-	while P != E:
-		# find longest difference set
-		longest_diffset = set()
-		for S_j in S:
-			diff = S_j - P
-			if len(diff) > len(longest_diffset):
-				longest_diffset = diff
+    P = set()
+    result = set()
+    while P != E:
+        # find longest difference set
+        longest_diffset = set()
+        for S_j in S:
+            diff = S_j - P
+            if len(diff) > len(longest_diffset):
+                longest_diffset = diff
 
-		P.update(longest_diffset)
-		result.add(Elem("S_" + "".join(map(str, longest_diffset)), longest_diffset))
+        P.update(longest_diffset)
+        result.add(Elem("S_" + "".join(map(str, longest_diffset)), longest_diffset))
 
-	return result
-
+    return result
 
 
 def minimum_k_and_d(T, E):
-	S = generate_S(T, E)
-	minimum_set = find_minimum_set_in_S_covering_E(E, S)
-	return minimum_set, len(minimum_set)
+    S = generate_S(T, E)
+    minimum_set = find_minimum_set_in_S_covering_E(E, S)
+    return minimum_set, len(minimum_set)
 
 
 def generate_E(V: set, H: set):
-	E = set()
-	for idx, v in enumerate(V):
-		e_i = set()
-		for h in H:
-			if v in h:
-				e_i.add(h)
-		E.add(Elem("E" + str(idx + 1), e_i, {v}))
+    E = set()
+    for idx, v in enumerate(V):
+        e_i = set()
+        for h in H:
+            if v in h:
+                e_i.add(h)
+        E.add(Elem("E" + str(idx + 1), e_i, {v}))
 
-	return E
+    return E
 
 
 def partitioner(numCPUs, cm, m, V, H):
-	min_max_L = cm
-	E = generate_E(V, H)
-	for d in range(m - cm):
-		S_star, k = minimum_k_and_d(generate_T(H, cm + d), E)
-		if k > numCPUs:
-			# create new E
-			E = set()
-			used_vertices = set()
-			for idx, s in enumerate(S_star):
-				E_i = set()
-				E_i_vertices = set()
-				for e in s:
-					E_i.update(e)
-					# add only vertices to the element E_i that aren't already used in another element E_j
-					diff = e.contained_vertices - used_vertices
-					used_vertices.update(diff)
-					E_i_vertices.update(diff)
-				E.add(Elem("E" + str(idx + 1), E_i, E_i_vertices))
+    min_max_L = cm
+    E = generate_E(V, H)
+    for d in range(m - cm):
+        S_star, k = minimum_k_and_d(generate_T(H, cm + d), E)
+        if k > numCPUs:
+            # create new E
+            E = set()
+            used_vertices = set()
+            for idx, s in enumerate(S_star):
+                E_i = set()
+                E_i_vertices = set()
+                for e in s:
+                    E_i.update(e)
+                    # add only vertices to the element E_i that aren't already used in another element E_j
+                    diff = e.contained_vertices - used_vertices
+                    used_vertices.update(diff)
+                    E_i_vertices.update(diff)
+                E.add(Elem("E" + str(idx + 1), E_i, E_i_vertices))
 
-		else:
-			min_max_L = cm + d
-			# extract partitions
-			V = set()
-			for idx, s in enumerate(S_star):
-				V_i = set()
-				for e in s:
-					V_i.update(e.contained_vertices)
-				V.add(Elem("V" + str(idx + 1), V_i))
+        else:
+            min_max_L = cm + d
+            # extract partitions
+            V = set()
+            for idx, s in enumerate(S_star):
+                V_i = set()
+                for e in s:
+                    V_i.update(e.contained_vertices)
+                V.add(Elem("V" + str(idx + 1), V_i))
 
-			return min_max_L, V
+            return min_max_L, V
 
 
 def partition_file_to_hypergraph(file_path):
-	V_raw, H_raw = converter.partition_file_to_hypergraph(file_path)
-	V = set(V_raw)
-	H = set()
-	for idx, h in enumerate(H_raw):
-		H.add(Elem("H" + str(idx + 1), set(h)))
+    V_raw, H_raw = converter.partition_file_to_hypergraph(file_path)
+    V = set(V_raw)
+    H = set()
+    for idx, h in enumerate(H_raw):
+        H.add(Elem("H" + str(idx + 1), set(h)))
 
-	return V, H
+    return V, H
 
 
 def print_ddf(k, blocks):
-	"""Print the split in the format of a data distribution file."""
-	print(k)
-	for i, block in enumerate(sorted(blocks, key=str)):
-		print('CPU' + str(i + 1), str(1))
-		print("partition_0", len(block), end=' ')
-		for l in block:
-			print(str(l), end=' ')
-		print()
+    """Print the split in the format of a data distribution file."""
+    print(k)
+    for i, block in enumerate(sorted(blocks, key=str)):
+        print('CPU' + str(i + 1), str(1))
+        print("partition_0", len(block), end=' ')
+        for l in block:
+            print(str(l), end=' ')
+        print()
 
 
 def main():
-	args = parse_arguments()
-	# V = {1, 2, 3, 4, 5, 6}
-	# H = {
-	# 	Elem("H1", {1, 2, 3}),
-	# 	Elem("H2", {4, 5}),
-	# 	Elem("H3", {2, 6}),
-	# 	Elem("H4", {3, 5, 6})
-	# }
-	V, H = partition_file_to_hypergraph(args.partition_file)
-	E = generate_E(V, H)
-	# maximum hyperdegree
-	cm = len(max(E, key=len))
-	m = len(H)
-	l, partitions = partitioner(args.k, cm, m, V, H)
-	print_ddf(args.k, partitions)
+    args = parse_arguments()
+    # V = {1, 2, 3, 4, 5, 6}
+    # H = {
+    # 	Elem("H1", {1, 2, 3}),
+    # 	Elem("H2", {4, 5}),
+    # 	Elem("H3", {2, 6}),
+    # 	Elem("H4", {3, 5, 6})
+    # }
+    V, H = partition_file_to_hypergraph(args.partition_file)
+    E = generate_E(V, H)
+    # maximum hyperdegree
+    cm = len(max(E, key=len))
+    m = len(H)
+    l, partitions = partitioner(args.k, cm, m, V, H)
+    print_ddf(args.k, partitions)
 
 
 if __name__ == "__main__":
-	main()
+    main()
