b2af9c08cd04a1d590d0226ff8272792dada188f
Peter Oettig <peet1993@gmail.com>
2018-09-13 11:15:29 +0200

    Added sdsl vector benchmark

--- JudiciousCppOptimized/Benchmark.cpp
+++ JudiciousCppOptimized/Benchmark.cpp
@@ -1,36 +1,47 @@
 #include <benchmark/benchmark.h>
 #include <boost/dynamic_bitset.hpp>
+#include <sdsl/bit_vectors.hpp>
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
 #include <random>
 #include <bitset>
 
+static const int SIZE = 5000000;
+
 static void bool_vector_create(benchmark::State &state) {
 	for (auto _ : state) {
-		std::vector<bool>(50000);
+		std::vector<bool> a(SIZE);
 	}
 }
 BENCHMARK(bool_vector_create);
 
 static void boost_dynamic_bitset_create(benchmark::State &state) {
 	for (auto _ : state) {
-		boost::dynamic_bitset<> a(50000, 0);
+		boost::dynamic_bitset<> a(SIZE, 0);
 	}
 }
 BENCHMARK(boost_dynamic_bitset_create);
 
-static void std_bitset_create(benchmark::State &state) {
+static void sdsl_bitvector_create(benchmark::State &state) {
 	for (auto _ : state) {
-		std::bitset<50000> a;
+		sdsl::bit_vector a(SIZE, 0);
 	}
 }
-BENCHMARK(std_bitset_create);
+BENCHMARK(sdsl_bitvector_create);
+
+static void sdsl_rrrvector_create(benchmark::State &state) {
+	for (auto _ : state) {
+		sdsl::bit_vector a0(SIZE, 0);
+		sdsl::rrr_vector<> a(a0);
+	}
+}
+BENCHMARK(sdsl_rrrvector_create);
 
 static void bool_vector_or(benchmark::State &state) {
-	std::vector<bool> a(50000);
-	std::vector<bool> b(50000);
-	std::vector<bool> c(50000);
+	std::vector<bool> a(SIZE);
+	std::vector<bool> b(SIZE);
+	std::vector<bool> c(SIZE);
 
 	for (auto _ : state) {
 		std::vector<bool>::iterator itC = c.begin();
@@ -40,14 +51,14 @@ static void bool_vector_or(benchmark::State &state) {
 		// c++ implementation-specific
 		while (itA < a.end())
 			*(itC._M_p ++) = *(itA._M_p ++) | *(itB._M_p ++); // word-at-a-time bitwise operation
-		}
+	}
 }
 BENCHMARK(bool_vector_or);
 
 static void bool_vector_or_stupid(benchmark::State &state) {
-	std::vector<bool> a(50000);
-	std::vector<bool> b(50000);
-	std::vector<bool> c(a.size());
+	std::vector<bool> a(SIZE);
+	std::vector<bool> b(SIZE);
+	std::vector<bool> c(SIZE);
 
 	for (auto _ : state) {
 		std::transform(a.begin(), a.end(),
@@ -57,9 +68,9 @@ static void bool_vector_or_stupid(benchmark::State &state) {
 BENCHMARK(bool_vector_or_stupid);
 
 static void boost_dynamic_bitset_or(benchmark::State &state) {
-	boost::dynamic_bitset<> a(50000, 0);
-	boost::dynamic_bitset<> b(50000, 0);
-	boost::dynamic_bitset<> c(50000, 0);
+	boost::dynamic_bitset<> a(SIZE, 0);
+	boost::dynamic_bitset<> b(SIZE, 0);
+	boost::dynamic_bitset<> c(SIZE, 0);
 
 	for (auto _ : state) {
 		c = a | b;
@@ -67,18 +78,40 @@ static void boost_dynamic_bitset_or(benchmark::State &state) {
 }
 BENCHMARK(boost_dynamic_bitset_or);
 
-static void std_bitset_or(benchmark::State &state) {
-	std::bitset<50000> a;
-	std::bitset<50000> b;
-	std::bitset<50000> c;
-
-	b[10] = true;
-	a[1000] = true;
+static void sdsl_bitvector_or(benchmark::State &state) {
+	sdsl::bit_vector a(SIZE, 0);
+	sdsl::bit_vector b(SIZE, 0);
+	sdsl::bit_vector c(SIZE, 0);
 
 	for (auto _ : state) {
-		c = a | b;
+		size_t i = 0;
+		for (; i + 64 < a.size(); i += 64) {
+			c.set_int(i, (a.get_int(i) | b.get_int(i)));
+		}
+		auto len = static_cast<uint8_t>(a.size() - i);
+		c.set_int(i, a.get_int(i, len) | b.get_int(i, len), len);
 	}
 }
-BENCHMARK(std_bitset_or);
+BENCHMARK(sdsl_bitvector_or);
+
+static void sdsl_bitvector_rrr_or(benchmark::State &state) {
+	sdsl::bit_vector a0(SIZE, 0);
+	sdsl::bit_vector b0(SIZE, 0);
+	sdsl::bit_vector c0(SIZE, 0);
+
+	sdsl::rrr_vector<> a(a0);
+	sdsl::rrr_vector<> b(b0);
+	sdsl::rrr_vector<> c(c0);
+
+	for (auto _ : state) {
+		size_t i = 0;
+		for (; i + 64 < a.size(); i += 64) {
+			c0.set_int(i, a.get_int(i) | b.get_int(i));
+		}
+		auto len = static_cast<uint8_t>(a.size() - i);
+		c0.set_int(i, a.get_int(i, len) | b.get_int(i, len), len);
+	}
+}
+BENCHMARK(sdsl_bitvector_rrr_or);
 
 BENCHMARK_MAIN();
