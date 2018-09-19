a7f8dd3f4bd58a48e124236850142dc18b05594d
peet <peet1993@gmail.com>
2018-09-19 20:21:43 +0200

    Added raw and singlepartition data for 59,128,404 and a script that generates that for supermatrix, because the file would be too large for git

---
+++ datasets/generate_supermatrix_all.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../RepeatsCounter/tools/MSAConverter/build/convert raw/supermatrix/supermatrix_C_nt2.fas raw/supermatrix/supermatrix_C_nt2_single.partitions raw/supermatrix/supermatrix_C_nt2.newick supermatrix_single.repeats
