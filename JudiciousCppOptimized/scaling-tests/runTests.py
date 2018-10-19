f8cee6bec464f8f0737a9821b58d3d20fc291e43
Lukas Hübner <huebnerlukas@gmail.com>
2018-10-19 15:20:29 +0200

    [ScalingTests] Sparsely test num_threads and use less max sites

--- JudiciousCppOptimized/scaling-tests/runTests.py
+++ JudiciousCppOptimized/scaling-tests/runTests.py
@@ -10,27 +10,31 @@ runtime_pattern = re.compile("Runtime: (\d+)ms")
 print("scaling,sites,k,threads,runtime")
 #for num_sites in range(config.MIN_SITES, config.MAX_SITES, config.STEP)
 
+def num_threads_generator():
+    num_threads = config.MIN_THREADS
+    while num_threads <= config.MAX_THREADS:
+        yield num_threads
+        num_threads *= config.THREAD_FACTOR
+
 # Strong scaling
 my_env = os.environ.copy()
-for num_threads in range(config.MIN_THREADS, config.MAX_THREADS):
+for num_threads in num_threads_generator():
     my_env["OMP_NUM_THREADS"] = str(num_threads)
     num_sites = config.STRONG_SCALING_NUM_SITES
     file_name = "%s/supermatrix_subsample_single_partiton_%d.repeats" % (config.REPEATS_DIR, num_sites)
     output = check_output([config.JUDICIOUS_BIN + "/JudiciousCpp", file_name, str(config.K)], env=my_env)
     runtime_line = output.split("\n".encode())[-2].decode()
     runtime = int(re.match(runtime_pattern, runtime_line).group(1))
-
     print("strong,%d,%d,%d,%d" % (num_sites, config.K, num_threads, runtime))
 
 # Weak scaling
 my_env = os.environ.copy()
-for num_threads in range(config.MIN_THREADS, config.MAX_THREADS):
+for num_threads in num_threads_generator():
     my_env["OMP_NUM_THREADS"] = str(num_threads)
     num_sites = num_threads * config.WEAK_SCALING_SITES_PER_CORE
     file_name = "%s/supermatrix_subsample_single_partiton_%d.repeats" % (config.REPEATS_DIR, num_sites)
     output = check_output([config.JUDICIOUS_BIN + "/JudiciousCpp", file_name, str(config.K)], env=my_env)
     runtime_line = output.split("\n".encode())[-2].decode()
     runtime = int(re.match(runtime_pattern, runtime_line).group(1))
-
     print("weak,%d,%d,%d,%d" % (num_sites, config.K, num_threads, runtime))
 
