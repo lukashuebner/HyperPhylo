f03468d094f7a99ee0028a3f3a8b9b7d57b9e22f
Lukas Hübner <huebnerlukas@gmail.com>
2018-10-29 11:57:31 +0100

    [ScalingTests] Improve test runner
    * Add parameters for machine, algorithm, dry-run, number of threads.
    * Autogenerate partitions and repeats files needed

--- JudiciousCppOptimized/scaling-tests/runTests.py
+++ JudiciousCppOptimized/scaling-tests/runTests.py
@@ -1,42 +1,92 @@
 #!/usr/bin/env python3
 
 import config
-from subprocess import check_output
+from subprocess import check_output, call
 import re
 import os
 import sys
+import itertools
+import argparse
 
 runtime_pattern = re.compile("Runtime: (\d+)ms")
 
-print("scaling,sites,k,threads,runtime")
-sys.stdout.flush()
+def generate_partitons_file(num_sites):
+    file_name = "%s/supermatrix_subsample_single_partiton_%d.partitions" % (config.PARTITIONS_DIR, num_sites)
+    if not os.path.isfile(file_name):
+        f = open(file_name, "w")
+        f.write("DNA, all = 1-%d" % num_sites)
+        f.close()
 
-def num_threads_generator():
-    num_threads = config.MIN_THREADS
-    while num_threads <= config.MAX_THREADS:
-        yield num_threads
-        num_threads *= config.THREAD_FACTOR
+def generate_repeats_file(num_sites):
+    input_file_name = "%s/supermatrix_subsample_single_partiton_%d.partitions" % (config.PARTITIONS_DIR, num_sites)
+    output_file_name = file_name.split('.')[0] + ".repeats"
+    if not os.path.isfile(output_file_name):
+        call([config.MSACONVERTER_BIN + "/convert",
+              config.SUPERMATRIX_DIR + "/supermatrix_C_nt2.fas",
+              config.PARTITIONS_DIR + "/" + input_file_name,
+              config.TREEFILE_DIR + "/supermatrix_C_nt2.newick",
+              config.REPEATS_DIR + "/" + output_file_name
+        ])
 
-# Strong scaling
-my_env = os.environ.copy()
-for num_threads in num_threads_generator():
+def strong_tests_generator(num_threads_list):
+    for num_threads in num_threads_list:
+        yield ("strong", num_threads, config.STRONG_SCALING_NUM_SITES, config.K)
+
+def weak_tests_generator(num_threads_list):
+    for num_threads in num_threads_list:
+        num_sites = num_threads * config.WEAK_SCALING_SITES_PER_CORE
+        yield ("weak", num_threads, num_sites, config.K)
+
+def measure_runtime(num_threads, num_sites, k):
+    my_env = os.environ.copy()
     my_env["OMP_NUM_THREADS"] = str(num_threads)
-    num_sites = config.STRONG_SCALING_NUM_SITES
     file_name = "%s/supermatrix_subsample_single_partiton_%d.repeats" % (config.REPEATS_DIR, num_sites)
-    output = check_output([config.JUDICIOUS_BIN + "/JudiciousCpp", file_name, str(config.K)], env=my_env)
+    output = check_output([config.JUDICIOUS_BIN + "/JudiciousCpp", file_name, str(k)], env=my_env)
     runtime_line = output.split("\n".encode())[-2].decode()
     runtime = int(re.match(runtime_pattern, runtime_line).group(1))
-    print("strong,%d,%d,%d,%d" % (num_sites, config.K, num_threads, runtime))
-    sys.stdout.flush()
+    return runtime
 
-# Weak scaling
-my_env = os.environ.copy()
-for num_threads in num_threads_generator():
-    my_env["OMP_NUM_THREADS"] = str(num_threads)
-    num_sites = num_threads * config.WEAK_SCALING_SITES_PER_CORE
-    file_name = "%s/supermatrix_subsample_single_partiton_%d.repeats" % (config.REPEATS_DIR, num_sites)
-    output = check_output([config.JUDICIOUS_BIN + "/JudiciousCpp", file_name, str(config.K)], env=my_env)
-    runtime_line = output.split("\n".encode())[-2].decode()
-    runtime = int(re.match(runtime_pattern, runtime_line).group(1))
-    print("weak,%d,%d,%d,%d" % (num_sites, config.K, num_threads, runtime))
-    sys.stdout.flush()
+def run_tests(dry_run, num_threads_list, printer):
+    test_params = itertools.chain(weak_tests_generator(num_threads_list), strong_tests_generator(num_threads_list))
+    for (scaling, num_threads, num_sites, k) in test_params:
+        if dry_run:
+            runtime = 0
+        else:
+            generate_partitions_file(num_sites)
+            generate_repeats_file(num_sites)
+            runtime = measure_runtime(num_threads, num_sites, k)
+        printer.print_result(scaling, num_sites, k, num_threads, runtime)
+
+class CSVPrinter:
+    """Prints the test results in csv format"""
+
+    _machine_id = ""
+    _algorithm = ""
+
+    def __init__(self, machine_id, algorithm, print_header):
+        self._machine_id = machine_id
+        self._algorithm = algorithm
+        if print_header:
+            self.print_header()
+
+    def print_header(self):
+        print("algorithm,scaling,machine,sites,k,threads,runtime")
+        sys.stdout.flush()
+
+    def print_result(self, scaling, num_sites, k, num_threads, runtime):
+        print("%s,%s,%s,%d,%d,%d,%d" % (self._algorithm, scaling, self._machine_id, num_sites, k, num_threads, runtime))
+        sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "Run scaling tests for JudiciousPartitioner.")
+    parser.add_argument("-p", "--print-header", action="store_true")
+    parser.add_argument("-m", "--machine-id", default="unknown")
+    parser.add_argument("-a", "--algorithm", default="unknown")
+    parser.add_argument("-d", "--dry-run", action="store_true")
+    parser.add_argument("-t", "--nthreads", type=int, nargs="+", default=[1, 2, 4, 8])
+    args = parser.parse_args()
+
+    printer = CSVPrinter(args.machine_id, args.algorithm, args.print_header)
+    run_tests(args.dry_run, args.nthreads, printer)
+
