b91cf1703fc4df2c1469bf838acbfaaf62214f4c
Lukas Hübner <huebnerlukas@gmail.com>
2019-01-11 10:11:08 +0100

    Forgot to save file

--- results/rdda_vs_hybrid/prettyFigures.R
+++ results/rdda_vs_hybrid/prettyFigures.R
@@ -2,9 +2,21 @@ library(ggplot2)
 library(dplyr)
 library(readr)
 
-data <- read_csv("Documents/Uni/SS18/BioInfo Praktikum/results/rdda_vs_hybrid/boxplot_data.csv")
+data <- read_csv(
+  file = "Documents/Uni/SS18/BioInfo Praktikum/results/rdda_vs_hybrid/boxplot_data.csv",
+  col_types = cols(
+    instance = col_character(),
+    algorithm = col_character(),
+    k = col_integer(),
+    coreRCC = col_integer()
+))
 
 data %>%
-  ggplot(aes(x = c(algorithm, instance), color = algorithm, y = coreRCC)) +
-  geom_boxplot()
+  filter(coreRCC != 0) %>%
+  ggplot(aes(x=instance, color=algorithm, y=coreRCC)) +
+  geom_boxplot(position = "dodge") +
+  #geom_jitter() +
+  scale_y_log10() +
+  theme_bw()
 
+t.test(coreRCC ~ )
